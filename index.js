0200587a7c7ee944d81e0a44bbfb85684f7bb108
Claire91290 <claire.91370@hotmail.fr>
2025-06-05 15:28:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-touch index.js
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connexion à MongoDB
+mongoose.connect('mongodb://localhost:27017/note-ton-logement', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
+db.once('open', () => {
+  console.log('Connecté à MongoDB');
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
