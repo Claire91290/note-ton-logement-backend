de6ea084058670bf7e65c648673b67d9ef4cb079
Claire91290 <claire.91370@hotmail.fr>
2025-06-07 18:51:27 +0400

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/api/ratings", (req, res) => {
 
   const current = data[entry.address];
 
-  ["secteur", "acces", "interieur", "exterieur", "loyer"].forEach((key) => {
+  ["secteur", "accès", "intérieur", "extérieur", "loyer"].forEach((key) => {
     const value = parseInt(entry[key]);
     if (!current.criteria[key]) current.criteria[key] = value;
     else current.criteria[key] = (current.criteria[key] + value) / 2;
