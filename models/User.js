015b9f96f2336a4c844eed109539648cb7ccd747
Claire91290 <claire.91370@hotmail.fr>
2025-06-26 22:14:39 +0400

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,10 @@
+// models/User.js
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  googleId: { type: String, required: true, unique: true },
+  email: String,
+  name: String,
+});
+
+export default mongoose.model("User", userSchema);
